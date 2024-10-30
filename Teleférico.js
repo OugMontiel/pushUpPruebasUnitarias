9aed36d68c3d7f2517f1846f70c5cfd08e10daa8
Diego Alejandro Montiel Florez <88790286+OugMontiel@users.noreply.github.com>
2024-10-30 11:19:48 -0500

    feat: :sparkles: teleferico Construido
    Falta validar si quedo bien :v jajajaj xD

--- Teleférico.js
+++ Teleférico.js
@@ -1,55 +1,119 @@
-import { Cabina } from "./Cabina";
-import { Usuario } from "./Usuario";
+const { Usuario } = require("./Usuario");
+const { Cabina } = require("./Cabina");
 
 export class Teleferico {
   constructor() {
     this.cabinas = [];
     this.usuarios = [];
   }
-
+  // get's
   getCabinas() {
     return this.cabinas;
   }
-
   getUsuarios() {
     return this.usuarios;
   }
-
-  crearCabina(id, capacidad) {
-    const nuevaCabina = new Cabina(id, capacidad);
-    this.cabinas.push(nuevaCabina);
-  }
-
-  eliminarCabina(id) {
-    this.cabinas = this.cabinas.filter((cabina) => cabina.id !== id);
-  }
-
-  asignarCabina(usuario) {
-    const cabinaDisponible = this.cabinas.find(
-      (cabina) =>
-        cabina.estado === "detenida" &&
-        cabina.pasajeros.length < cabina.capacidad
-    );
-    if (cabinaDisponible) {
-      cabinaDisponible.agregarPasajero(usuario);
-      return `Usuario ${usuario.nombre} asignado a cabina ${cabinaDisponible.id}`;
-    }
-    return "No hay cabinas disponibles.";
-  }
-
-  solicitarViaje(usuario, cabina) {
-    // Implementar la lógica para verificar si el usuario puede viajar en la cabina
-    // Verificar si la cabina tiene espacio disponible y si el usuario cumple con los requisitos de edad
-    // Si todo está correcto, permitir el viaje y actualizar el estado de la cabina a "en movimiento"
-    // De lo contrario, informar al usuario que no hay espacio disponible o que no cumple con los requisitos de edad
-  }
-
-  listarCabinas() {
+  getListarCabinas() {
     return this.cabinas.map((cabina) => ({
       id: cabina.id,
       estado: cabina.estado,
       capacidad: cabina.capacidad,
       ocupacion: cabina.pasajeros.length,
+      pasajeros: cabina.pasajeros,
     }));
   }
+  getListarUsuarios() {
+    return this.usuarios.map((usuario) => ({
+      id: usuario.id,
+      nombre: usuario.nombre,
+      edad: usuario.edad,
+    }));
+  }
+  getListarPasajerosCabina(cabinaId) {
+    const cabina = this.cabinas.find((cabina) => cabina.id === cabinaId);
+    if (cabina) {
+      return cabina.pasajeros.map((pasajero) => ({
+        id: pasajero.id,
+        nombre: pasajero.nombre,
+        edad: pasajero.edad,
+      }));
+    }
+    return `Cabina ${cabinaId} no encontrada.`;
+  }
+  
+
+  // set's Cabinas
+  crearCabina(cabinaId, capacidad) {
+    const nuevaCabina = new Cabina(cabinaId, capacidad);
+    this.cabinas.push(nuevaCabina);
+  }
+  eliminarCabina(cabinaId) {
+    this.cabinas = this.cabinas.filter((cabina) => cabina.id !== cabinaId);
+  }
+  iniciarCabina(cabinaId) {
+    const cabina = this.cabinas.find((cabina) => cabina.id === cabinaId);
+    if (cabina) {
+      cabina.mover(); // Llama al método mover de la clase Cabina
+      return `Cabina ${cabina.id} iniciada.`;
+    }
+    return `Cabina ${id} no encontrada.`;
+  }
+  detenerCabina(cabinaId) {
+    const cabina = this.cabinas.find((cabina) => cabina.id === cabinaId);
+    if (cabina) {
+      cabina.detener(); // Llama al método detener de la clase Cabina
+      return `Cabina ${cabina.id} detenida.`;
+    }
+    return `Cabina ${id} no encontrada.`;
+  }
+  obtenerCabinasDisponibles() { 
+    return this.cabinas
+      .filter(
+        (cabina) =>
+          cabina.estado === "detenida" &&
+          cabina.pasajeros.length < cabina.capacidad
+      )
+      .map((cabina) => cabina.id);
+  }
+
+  // set's Usuarios
+  crearUsuario(usuarioId, nombre, edad) {
+    const nuevoUsuario = new Usuario(usuarioId, nombre, edad);
+    this.usuarios.push(nuevoUsuario);
+    return nuevoUsuario; // Devuelve el usuario creado
+  }
+  eliminarUsuario(usuarioId) {
+    const index = this.usuarios.findIndex(
+      (usuario) => usuario.id === usuarioId
+    );
+    if (index !== -1) {
+      this.usuarios.splice(index, 1);
+      return true; // Usuario eliminado con éxito
+    }
+    return false; // Usuario no encontrado
+  }
+  obtenerUsuariosDisponibles() {
+    const usuariosEnCabinaIds = this.cabinas.flatMap((cabina) =>
+      cabina.pasajeros.map((pasajero) => pasajero.id)
+    );
+
+    return this.usuarios
+      .filter((usuario) => !usuariosEnCabinaIds.includes(usuario.id))
+      .map((usuario) => usuario.id);
+  }// Verificar Disponibilidad de Usuarios: Un método que retorne los id de las usuarios disponibles(que no esten en cabina).
+
+  // set's interacion usuario y cabinas
+  agregarPasajeroACabina(usuarioId, cabinaId) {
+    const usuario = this.usuarios.find((usuario) => usuario.id === usuarioId);
+    const cabina = this.cabinas.find((cabina) => cabina.id === cabinaId);
+    
+    if (!usuario) return "Usuario no encontrado.";
+    if (!cabina) return "Cabina no encontrada.";
+
+    if (cabina.pasajeros.length < cabina.capacidad) {
+      cabina.agregarPasajero(usuario);
+      return `Usuario ${usuario.nombre} agregado a la cabina ${cabina.id}.`;
+    }
+    return "Cabina llena.";
+  } // Agregar Pasajero a Cabina: Un método que permita agregar un pasajero a una cabina
 }
